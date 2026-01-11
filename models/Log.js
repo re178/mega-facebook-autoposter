a2e66f5d074603da36bb71053346e187891c7fad
re178 <esilesirayland@gmail.com>
2026-01-11 18:35:49 +0300

    Add Log model with schema for logging actions

--- models/Log.js
+++ models/Log.js
@@ -1,1 +1,23 @@
+const mongoose = require('mongoose');
+
+const LogSchema = new mongoose.Schema({
+  pageId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Page'
+  },
+  action: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    default: ''
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Log', LogSchema);
 
