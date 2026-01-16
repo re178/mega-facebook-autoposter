8dae2b18d98610cca9cf1cd9d078713dc2eef4ce
re178 <esilesirayland@gmail.com>
2026-01-16 16:43:09 +0300

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
+
 const CommentSchema = new mongoose.Schema({
   pageId: { type: mongoose.Schema.Types.ObjectId, ref: 'Page' },
+
+  facebookCommentId: { type: String, index: true }, // ✅ REQUIRED
+
   userName: String,
   comment: String,
+
   postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
+
   createdAt: { type: Date, default: Date.now },
   status: { type: String, enum:['VISIBLE','HIDDEN'], default:'VISIBLE' }
 });
+
 module.exports = mongoose.model('Comment', CommentSchema);
