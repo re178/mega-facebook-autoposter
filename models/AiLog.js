007b83f1b355d51564ea7ef67c1774aaea598ba1
re178 <esilesirayland@gmail.com>
2026-01-28 12:27:06 +0300

    Update AiLog.js

--- models/AiLog.js
+++ models/AiLog.js
@@ -7,12 +7,26 @@ const AiLogSchema = new mongoose.Schema({
     required: true
   },
   pageId: {
+    type: String, 
+    required: true
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'AiScheduledPost',
+    default: null
+  },
+  action: {
     type: String,
     required: true
   },
-  action: String,
-  message: String,
-  createdAt: { type: Date, default: Date.now }
+  message: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('AiLog', AiLogSchema);
