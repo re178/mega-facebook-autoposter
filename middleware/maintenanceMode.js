2ad50cceff474f56879a5424d0e3e75b91be6675
re178 <esilesirayland@gmail.com>
2026-05-15 21:47:26 +0300

    Update maintenanceMode.js

--- middleware/maintenanceMode.js
+++ middleware/maintenanceMode.js
@@ -1,1 +1,19 @@
+const SystemSettings = require('../models/SystemSettings');
 
+module.exports = async function maintenanceMode(req, res, next) {
+    try {
+        const settings = await SystemSettings.findOne();
+
+        if (settings && settings.maintenanceMode === true) {
+            return res.status(503).json({
+                error: 'System is currently under maintenance'
+            });
+        }
+
+        next();
+
+    } catch (err) {
+        console.error(err);
+        next();
+    }
+};
