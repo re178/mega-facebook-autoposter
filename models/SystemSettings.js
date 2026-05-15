c95e3ed58b6e317705d424d66b0958717d92764b
re178 <esilesirayland@gmail.com>
2026-05-15 21:44:40 +0300

    Add SystemSettings model with maintenanceMode field

--- models/SystemSettings.js
+++ models/SystemSettings.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
 
+const SystemSettingsSchema = new mongoose.Schema({
+
+    maintenanceMode: {
+        type: Boolean,
+        default: false
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model('SystemSettings', SystemSettingsSchema);
