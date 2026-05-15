cf6f2d0250a88bdf9aecb68161aace5c7b2359db
re178 <esilesirayland@gmail.com>
2026-05-15 21:41:58 +0300

    Update BroadcastMessage.js

--- models/BroadcastMessage.js
+++ models/BroadcastMessage.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
 
+const BroadcastMessageSchema = new mongoose.Schema({
+
+    message: {
+        type: String,
+        required: true
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model('BroadcastMessage', BroadcastMessageSchema);
