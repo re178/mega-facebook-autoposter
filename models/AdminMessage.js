b37dd0b2464c34db95d49a3eac424400b73fbf6e
re178 <esilesirayland@gmail.com>
2026-05-15 21:43:12 +0300

    Add AdminMessage schema for MongoDB

--- models/AdminMessage.js
+++ models/AdminMessage.js
@@ -1,1 +1,28 @@
+const mongoose = require('mongoose');
 
+const AdminMessageSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    message: {
+        type: String,
+        required: true
+    },
+
+    read: {
+        type: Boolean,
+        default: false
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model('AdminMessage', AdminMessageSchema);
