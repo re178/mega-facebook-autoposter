0f382fa4650c1e4d95056a5416f629a029f124d2
re178 <esilesirayland@gmail.com>
2026-05-15 21:50:45 +0300

    Update requireLogin.js

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,1 +1,9 @@
+module.exports = function requireLogin(req, res, next) {
+    if (req.session && req.session.userId) {
+        return next();
+    }
 
+    return res.status(401).json({
+        error: 'Unauthorized'
+    });
+};
