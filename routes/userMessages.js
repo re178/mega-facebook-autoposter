b490d1be5ec599af0552457d1bc8941bce2c1815
re178 <esilesirayland@gmail.com>
2026-05-18 13:53:46 +0300

    Update userMessages.js

--- routes/userMessages.js
+++ routes/userMessages.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const requireLogin = require('../middleware/requireLogin');
-const User = require('./models/User');
+const User = require('../models/User');
 const AdminMessage = require('../models/AdminMessage');
 const BroadcastMessage = require('../models/BroadcastMessage');
 
