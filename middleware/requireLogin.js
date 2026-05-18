cc6ed68b39f0a526fa0f8c9ce9d0093e9e8d21ca
re178 <esilesirayland@gmail.com>
2026-05-18 20:56:55 +0300

    Update requireLogin.js

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,3 +1,4 @@
+const messages = await AdminMessage.find({ userId: req.user._id }) // ✅ Works now
 module.exports = function requireLogin(req, res, next) {
     if (req.session && req.session.userId) {
         return next();
